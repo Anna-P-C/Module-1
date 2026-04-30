55d7d704ea19c60d922c621f473651503ed24254
Anna Polyakova <ipz242_pas@student.ztu.edu.ua>
2026-04-30 21:11:39 +0300

    Pattern: Visitor implementation

--- kpz_lab.3.5/kpz_lab.3.5.cs
+++ kpz_lab.3.5/kpz_lab.3.5.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections;
 
 namespace Lab3_Composite
 {
@@ -11,109 +12,78 @@ namespace Lab3_Composite
             Console.OutputEncoding = Encoding.UTF8;
 
             var table = new LightElementNode("table", "block", "double");
-            table.AddClass("data-table");
-
             var tr = new LightElementNode("tr", "block", "double");
+            var th = new LightElementNode("th", "inline", "double");
+            th.AddChild(new LightTextNode("Назва"));
 
-            var th1 = new LightElementNode("th", "inline", "double");
-            th1.AddChild(new LightTextNode("Назва"));
-
-            var th2 = new LightElementNode("th", "inline", "double");
-            th2.AddChild(new LightTextNode("Ціна"));
-
-            tr.AddChild(th1);
-            tr.AddChild(th2);
+            tr.AddChild(th);
             table.AddChild(tr);
 
-            var row = new LightElementNode("tr", "block", "double");
-            var td1 = new LightElementNode("td", "inline", "double");
-            td1.AddChild(new LightTextNode("Яблуко"));
+            Console.WriteLine("--- Тест Visitor  ---");
+            var visitor = new ElementCounterVisitor();
+            table.Accept(visitor);
 
-            var td2 = new LightElementNode("td", "inline", "double");
-            td2.AddChild(new LightTextNode("30 грн"));
-
-            row.AddChild(td1);
-            row.AddChild(td2);
-            table.AddChild(row);
-
-            var img = new LightElementNode("img", "inline", "single");
-            img.AddClass("product-image");
-            table.AddChild(img);
-
-            Console.WriteLine("Демонстрація OuterHTML ");
-            Console.WriteLine(table.OuterHTML());
-
-            Console.WriteLine("\nДемонстрація InnerHTML ");
-            Console.WriteLine(table.InnerHTML());
+            Console.WriteLine($"Всього HTML-тегів знайдено: {visitor.Count}");
 
             Console.ReadKey();
         }
     }
 
+    public interface IVisitor
+    {
+        void VisitTextNode(LightTextNode node);
+        void VisitElementNode(LightElementNode node);
+    }
+
+    public class ElementCounterVisitor : IVisitor
+    {
+        public int Count { get; private set; }
+        public void VisitTextNode(LightTextNode node) { }
+        public void VisitElementNode(LightElementNode node) => Count++;
+    }
+
     public abstract class LightNode
     {
-        public abstract string OuterHTML();
-        public abstract string InnerHTML();
+        public abstract string Render();
+        public abstract void Accept(IVisitor visitor);
     }
 
     public class LightTextNode : LightNode
     {
         private string _text;
         public LightTextNode(string text) => _text = text;
-        public override string OuterHTML() => _text;
-        public override string InnerHTML() => _text;
+        public override string Render() => _text;
+        public override void Accept(IVisitor visitor) => visitor.VisitTextNode(this);
     }
 
     public class LightElementNode : LightNode
     {
         private string _tagName;
-        private string _displayType;
-        private string _closingType;
-        private List<string> _classes = new List<string>();
         private List<LightNode> _children = new List<LightNode>();
 
         public LightElementNode(string tag, string display, string closing)
         {
             _tagName = tag;
-            _displayType = display;
-            _closingType = closing;
         }
 
-        public void AddClass(string className) => _classes.Add(className);
         public void AddChild(LightNode node) => _children.Add(node);
 
-        public override string InnerHTML()
+        public override string Render()
         {
             StringBuilder sb = new StringBuilder();
-            foreach (var child in _children)
-            {
-                sb.Append(child.OuterHTML());
-            }
+            sb.Append($"<{_tagName}>");
+            foreach (var child in _children) sb.Append(child.Render());
+            sb.Append($"</{_tagName}>");
             return sb.ToString();
         }
 
-        public override string OuterHTML()
+        public override void Accept(IVisitor visitor)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append($"<{_tagName}");
-
-            if (_classes.Count > 0)
+            visitor.VisitElementNode(this);
+            foreach (var child in _children)
             {
-                sb.Append($" class=\"{string.Join(" ", _classes)}\"");
+                child.Accept(visitor);
             }
-
-            if (_closingType == "single")
-            {
-                sb.Append(" />");
-            }
-            else
-            {
-                sb.Append(">");
-                sb.Append(InnerHTML());
-                sb.Append($"</{_tagName}>");
-            }
-
-            return sb.ToString();
         }
     }
 }
